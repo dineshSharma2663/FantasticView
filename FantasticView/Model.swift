54261655b92675b17d306b076290f8d11a944438
dinesh sharma <dineshsharma@dineshs-Mac-mini.local>
2017-05-23 19:01:19 +0530

    init

---
+++ FantasticView/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  FantasticView
+//
+//  Created by dinesh sharma on 23/05/17.
+//  Copyright © 2017 dinesh sharma. All rights reserved.
+//
+
+
+import UIKit
+
+
+extension UIView{
+    
+    func setBackColor() {
+        self.backgroundColor = UIColor.red
+    }
+}
